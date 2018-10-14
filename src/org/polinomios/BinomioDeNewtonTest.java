cb6ba80a31b12ee8e00afd78f8b368ecc44b9a3a
Matias De Stefano <matiasdestefano97@gmail.com>
2018-10-14 15:06:30 -0300

    Tests BinomioDeNewton corregidos

--- src/org/polinomios/BinomioDeNewtonTest.java
+++ src/org/polinomios/BinomioDeNewtonTest.java
@@ -13,73 +13,37 @@ class BinomioDeNewtonTest {
 
 	@Test
 	void obtenerCoeficienteKSinPow() { // No se emplea método Math.pow();
-		long startTime;
-		long finishTime;
-		long totalTime;
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
-		startTime = System.nanoTime();
 		Assert.assertEquals(13720, binomio.obtenerCoeficienteKSinPow(2), 0);
-		finishTime = System.nanoTime();
-		totalTime = finishTime - startTime;
-		System.out.println("obtenerCoeficienteKSinPow:\t" + totalTime + "ns");
 		// Sin emplear el método Pow, el rendimiento mejora notablemente.
 	}
 
 	@Test
 	void obtenerCoeficienteKRecursivo() {
-		long startTime;
-		long finishTime;
-		long totalTime;
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
-		startTime = System.nanoTime();
 		Assert.assertEquals(13720, binomio.obtenerCoeficienteKRecursivo(2), 0);
-		finishTime = System.nanoTime();
-		totalTime = finishTime - startTime;
-		System.out.println("obtenerCoeficienteKSinPow:\t" + totalTime + "ns");
 		// Tarda menos que con pow pero mas que con la potencia directa
 	}
 
 	@Test
 	void obtenerTodosLosCoeficientes() {
-		long startTime;
-		long finishTime;
-		long totalTime;
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		double[] coeficientes = new double[] { 32, 560, 3920, 13720, 24010, 16807 };
-		startTime = System.nanoTime();
 		Assert.assertArrayEquals(coeficientes, binomio.obtenerCoeficientes(), 0);
-		finishTime = System.nanoTime();
-		totalTime = finishTime - startTime;
-		System.out.println("obtenerTodosLosCoeficientes:\t" + totalTime + "ns");
 	}
 
 	@Test
 	void testCoeficientesSinPow() {
-		long startTime;
-		long finishTime;
-		long totalTime;
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		double[] coeficientes = new double[] { 32, 560, 3920, 13720, 24010, 16807 };
-		startTime = System.nanoTime();
 		Assert.assertArrayEquals(coeficientes, binomio.obtenerCoeficientesSinPow(), 0);
-		finishTime = System.nanoTime();
-		totalTime = finishTime - startTime;
-		System.out.println("obtenerTodosLosCoeficientes2:\t" + totalTime + "ns");
-		// Al ser un binomio de mayor grado tarda más tiempo en realizar el cálculo.
 	}
 
 	@Test
 	void testCoeficienteRecursivo() {
-		long startTime;
-		long finishTime;
-		long totalTime;
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		double[] coeficientes = new double[] { 32, 560, 3920, 13720, 24010, 16807 };
-		startTime = System.nanoTime();
 		Assert.assertArrayEquals(coeficientes, binomio.obtenerCoeficientesRecursivo(), 0);
-		finishTime = System.nanoTime();
-		totalTime = finishTime - startTime;
-		System.out.println("obtenerTodosLosCoeficientesRecursivo:\t" + totalTime + "ns");
 	}
 
 	@Test
