a2bf14c92e913832dca49acd104800b22e0cbf37
Matias De Stefano <matiasdestefano97@gmail.com>
2018-10-14 15:17:16 -0300

    Tiempos Listos para testear

--- src/org/polinomios/BinomioDeNewtonTest.java
+++ src/org/polinomios/BinomioDeNewtonTest.java
@@ -11,28 +11,28 @@ class BinomioDeNewtonTest {
 		Assert.assertEquals(13720, binomio.obtenerCoeficienteK(2), 0.1);
 	}
 
-	@Test
+	//@Test
 	void obtenerCoeficienteKSinPow() { // No se emplea método Math.pow();
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		Assert.assertEquals(13720, binomio.obtenerCoeficienteKSinPow(2), 0);
 		// Sin emplear el método Pow, el rendimiento mejora notablemente.
 	}
 
-	@Test
+	//@Test
 	void obtenerCoeficienteKRecursivo() {
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		Assert.assertEquals(13720, binomio.obtenerCoeficienteKRecursivo(2), 0);
 		// Tarda menos que con pow pero mas que con la potencia directa
 	}
 
-	@Test
+	//@Test
 	void obtenerTodosLosCoeficientes() {
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		double[] coeficientes = new double[] { 32, 560, 3920, 13720, 24010, 16807 };
 		Assert.assertArrayEquals(coeficientes, binomio.obtenerCoeficientes(), 0);
 	}
 
-	@Test
+	//@Test
 	void testCoeficientesSinPow() {
 
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
@@ -41,23 +41,10 @@ class BinomioDeNewtonTest {
 		// Al ser un binomio de mayor grado tarda más tiempo en realizar el cálculo.
 	}
 
-	@Test
+	//@Test
 	void testCoeficienteRecursivo() {
 		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
 		double[] coeficientes = new double[] { 32, 560, 3920, 13720, 24010, 16807 };
 		Assert.assertArrayEquals(coeficientes, binomio.obtenerCoeficientesRecursivo(), 0);
 	}
-
-	@Test
-	void testPotencia() {
-		BinomioDeNewton binomio = new BinomioDeNewton(7, 2, 5);
-		Assert.assertEquals(1048576, binomio.potencia(4, 10), 0);
-	}
-
-	@Test
-	void testPotenciaRecursiva() {
-		BinomioDeNewton bn = new BinomioDeNewton(7, 2, 5);
-		Assert.assertEquals(1048576, bn.potenciaRecursiva(4, 10), 0);
-	}
-
 }
